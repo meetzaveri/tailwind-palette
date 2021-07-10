fff5f39a6159517ab86b22e9144146a7d2613754
meetzaveri <meethpatel96@gmail.com>
2021-07-10 16:38:15 +0530

    chore: update ts config and added packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    light: {
+                        100: '#8993A4',
+                        200: '#97A0AF',
+                    },
+                    dark: {
+                        100: '#172B4D',
+                        200: '#253858',
+                    },
+                },
+            },
+        },
     },
     variants: {
         extend: {},
